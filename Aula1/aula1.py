5ab36dd65bcd4102785d8670f15e18e87821c748
vando <vanderson.castro@ufv.br>
2021-03-16 19:37:26 -0300

    Subindo Curso

---
+++ Aula1/aula1.py
@@ -0,0 +1,10 @@
+print('Python 3 completo - '
+      'programação com Django, PyQT5, '
+      'Selenium, Regexp, Testes e TDD, POO, '
+      'Design Patterns GoF, algoritmos')
+
+print('Pelos números Nível de experiência: '
+      'Todos os níveis Alunos: '
+      '21769 Idiomas: '
+      'Português Legendas: Sim Aulas: 411 '
+      'Vídeo: 109,5 horas no total.')
